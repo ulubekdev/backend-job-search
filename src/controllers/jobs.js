a62b03c57425d6a51efd8bb26b8f869ea01efa04
Ulug'bek <93920465+theulubek@users.noreply.github.com>
2022-08-19 16:40:14 +0500

    create job

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -13,8 +13,46 @@ const GET_JOBS = async (req, res, next) => {
     }
 };
 
+const CREATE_JOB = async (req, res, next) => {
+    try {
+        const {
+            title,
+            company,
+            description,
+            location,
+            experience,
+            salary,
+            type,
+            category,
+            skills,
+            requirements,
+            status
+        } = req.body;
+
+        if(!title || !company || !description || !location || !type || !category) {
+            return res.status(400).send({
+                status: 400,
+                message: 'Please fill all required fields',
+                data: null
+            });
+        }
+
+        const job = await req.models.Job.create({
+            title, company, description, location, experience, salary, type, category, skills, requirements, status
+        });
+        res.send({
+            status: 201,
+            message: 'Job created successfully',
+            data: job
+        });
+    } catch (error) {
+        return next(new InternalServerError(500, error.message));
+    }
+
+};
 
 
 export default {
-    GET_JOBS
+    GET_JOBS,
+    CREATE_JOB
 }
--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -6,7 +6,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/jobs', controllers.GET_JOBS);
-// router.post('/jobs', checkToken, checkAdmin, controllers.CREATE_JOB);
+router.post('/jobs', checkToken, checkAdmin, controllers.CREATE_JOB);
 // router.put('/jobs/:id', checkToken, checkAdmin, controllers.UPDATE_JOB);
 // router.delete('/jobs/:id', checkToken, checkAdmin, controllers.DELETE_JOB);
 
